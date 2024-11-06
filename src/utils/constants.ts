a1290db896176ad31a82018727931a6f6910c9a4
Freddie Runnding <freddie.runnding@gmail.com>
2024-11-06 16:06:57 +0100

    added constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+export const CINODE_API = {
+  BASE_URL: 'https://api.cinode.com',
+  TOKEN_URL: 'https://api.cinode.com/token',
+  COMPANY_ID: 175,
+  HEADERS: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+} as const;
+
+export enum ProjectDefaults {
+  CUSTOMER_ID = 158342, // SalesAid Tretton37
+  PIPELINE_ID = 3020, // Sales Aid - Broker ads
+  PIPELINE_STAGE_ID = 14458, // Incoming
+  CURRENCY_ID = 1, // SEK
+  SALES_MANAGER_ID = 228236, // SalesAid Tretton37 user
+}
+
+export enum Priority {
+  LOW = 1,
+  MEDIUM = 5,
+  HIGH = 10,
+}
