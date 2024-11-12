3e3917225a4593c1d7ba25822c15dc6eb105dd12
Richard Davy <richard.davy.tretton37@outlook.com>
2024-11-12 14:48:21 +0100

    upd cloudbuild and added google secrets

--- src/app.js
+++ src/app.js
@@ -5,32 +5,33 @@ dotenv.config();
 const { App } = pkg;
 import { Botkit } from 'botkit';
 import { SlackAdapter } from 'botbuilder-adapter-slack';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 
-// /**
-//  * Returns the secret string from Google Cloud Secret Manager
-//  * @param {string} name The name of the secret.
-//  * @return {Promise<string>} The string value of the secret.
-//  */
-// async function accessSecretVersion(name) {
-//   const client = new SecretManagerServiceClient();
-//   const projectId = process.env.PROJECT_ID;
-//   const [version] = await client.accessSecretVersion({
-//     name: `projects/${projectId}/secrets/${name}/versions/1`,
-//   });
+/**
+ * Returns the secret string from Google Cloud Secret Manager
+ * @param {string} name The name of the secret.
+ * @return {Promise<string>} The string value of the secret.
+ */
+async function accessSecretVersion(name) {
+  const client = new SecretManagerServiceClient();
+  const projectId = process.env.PROJECT_ID;
+  const [version] = await client.accessSecretVersion({
+    name: `projects/${projectId}/secrets/${name}/versions/1`,
+  });
 
-//   // Extract the payload as a string.
-//   const payload = version.payload.data.toString('utf8');
+  // Extract the payload as a string.
+  const payload = version.payload.data.toString('utf8');
 
-//   return payload;
-// }
+  return payload;
+}
 
 
 
 
 const adapter = new SlackAdapter({
-  clientSigningSecret: process.env.SLACK_SIGNING_SECRET,
-  botToken: process.env.SLACK_BOT_TOKEN,
+  clientSigningSecret: await accessSecretVersion('client-signing-secret'),
+  botToken: await accessSecretVersion('bot-token'),
 });
 
 
