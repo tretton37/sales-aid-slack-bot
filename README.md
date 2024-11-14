2014399ccd082c95af260ed857eb01d4f17e7656
Richard Davy <richard.davy.tretton37@outlook.com>
2024-11-14 09:22:00 +0100

    Changed deploy to use docker container instead of cloudbuild

--- README.md
+++ README.md
@@ -58,12 +58,45 @@ Enable events under 'Event Subscriptions' in Slack app admin.
 
 2. The bot will start running and listen for events on the specified port (default is 3000).
 
+## Deploy
+
+The bot is deployed to Google Cloud using the docker container.
+
+1. Make sure you have [Docker](https://www.docker.com/get-started/) installed.
+2. Install the [Google Cloud CLI](https://cloud.google.com/sdk/docs/install).
+3. Create a Google Cloud project (either through portal or CLI)
+4. Log in to your google cloud via the CLI
+   ```bash
+   gcloud auth login
+   gcloud config set project [YOUR_PROJECT_ID]
+   ```
+5. Athenticate docker to use your google auth
+   ```bash
+   gcloud auth configure-docker
+   ```
+6. Build the image.
+   ```bash
+   docker build -t gcr.io/[YOUR_PROJECT_ID]/slack-bolt-app .
+   ```
+7. Push the image to Google Container Registry
+   ```bash
+   docker push gcr.io/[YOUR_PROJECT_ID]/slack-bolt-app
+   ```
+8. Deploy the Google Run Function
+   ```bash
+   gcloud run deploy slack-bolt-app \
+   --image gcr.io/[YOUR_PROJECT_ID]/slack-bolt-app \
+   --platform managed \
+   --region [YOUR_REGION] \
+   --allow-unauthenticated \
+   --set-env-vars PROJECT_ID = [YOUR_PROJECT_ID]
+   ```
+
 ## Dependencies
 
 - [@slack/bolt](https://www.npmjs.com/package/@slack/bolt) - Slack app framework
 - [@slack/socket-mode](https://www.npmjs.com/package/@slack/socket-mode) - Slack Socket Mode client
 - [@slack/web-api](https://www.npmjs.com/package/@slack/web-api) - Slack Web API client
-- [express](https://www.npmjs.com/package/express) - Web framework for Node.js
 
 ## License
 
--- src/app.js
+++ src/app.js
@@ -27,40 +27,22 @@ async function accessSecretVersion(name) {
 }
 
 
+const app = new App({
+  // appToken: process.env.SLACK_APP_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // socketMode: false,
+  port: process.env.PORT || 3000, // Currently not used as in socket mode.
 
-const adapter = new SlackAdapter({
-  clientSigningSecret: await accessSecretVersion('client-signing-secret'),
-  botToken: await accessSecretVersion('bot-token'),
 });
 
+// Start the app
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
 
+// Register message handlers
+app.event('app_mention', handleGreetingMessage);
+app.message('From', handleNewCinodeMarketAnnouncement);
 
-const controller = new Botkit({
-  adapter: adapter,
-});
-
-
-
-// const app = new App({
-//   appToken: process.env.SLACK_APP_TOKEN,
-//   token: process.env.SLACK_BOT_TOKEN,
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   socketMode: true,
-//   port: process.env.PORT || 3000, // Currently not used as in socket mode.
-// });
-
-// // Start the app
-// (async () => {
-//   await app.start(process.env.PORT || 3000);
-//   console.log('⚡️ Bolt app is running!');
-// })();
-
-// // Register message handlers
-// app.event('app_mention', handleGreetingMessage);
-// app.message('From', handleNewCinodeMarketAnnouncement);
-
-
-controller.ready(() => {
-  controller.hears('hello','message', async (bot, message) => { await bot.reply(message, 'hello');})
-  controller.hears('From', 'message', handleNewCinodeMarketAnnouncement2)
-});
