f3bd4866c3076203f9abed70ea91b9c2731a5da7
Freddie Runnding <freddie.runnding@gmail.com>
2024-11-15 12:36:15 +0100

    removed unused packages

--- src/app.js
+++ src/app.js
@@ -1,29 +1,21 @@
-import pkg from '@slack/bolt';
 import dotenv from 'dotenv';
-import { handleGreetingMessage, handleNewCinodeMarketAnnouncement, handleNewCinodeMarketAnnouncement2 } from './handlers/messageHandlers.js';
-import { Botkit } from 'botkit';
-import { SlackAdapter } from 'botbuilder-adapter-slack';
 dotenv.config();
+import { handleGreetingMessage, handleNewCinodeMarketAnnouncement } from './handlers/messageHandlers.js';
+
+import pkg from '@slack/bolt';
 const { App } = pkg;
 
-
-
 const app = new App({
-  // appToken: process.env.SLACK_APP_TOKEN,
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // socketMode: false,
-  port: process.env.PORT || 3000, // Currently not used as in socket mode.
-
 });
+
+// Register message handlers
+app.event('app_mention', handleGreetingMessage);
+app.message('From', handleNewCinodeMarketAnnouncement);
 
 // Start the app
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
-
-// Register message handlers
-app.event('app_mention', handleGreetingMessage);
-app.message('From', handleNewCinodeMarketAnnouncement);
-
